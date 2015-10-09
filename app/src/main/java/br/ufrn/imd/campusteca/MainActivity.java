7f0b8e4321e085ca9f4efb345c8a722b3fcbdfae
Pablo Gabriel <pablo.reds@hotmail.com>
2015-10-08 23:48:14 -0300

    Adding listview in MainActivity #2

--- app/src/main/java/br/ufrn/imd/campusteca/MainActivity.java
+++ app/src/main/java/br/ufrn/imd/campusteca/MainActivity.java
@@ -1,19 +1,28 @@
 package br.ufrn.imd.campusteca;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.ufrn.imd.campusteca.adapter.ListViewAdapter;
+import br.ufrn.imd.campusteca.model.ListViewItem;
 
 public class MainActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener {
 
     private Toolbar toolbar;
     private FragmentDrawer drawer;
+
+    private ListView listView;
+    private ListViewAdapter listViewAdapter;
+    private List<ListViewItem> itens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +35,37 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
         drawer = (FragmentDrawer) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawer.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
         drawer.setDrawerListener(this);
+
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, ArchiveActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        populateListView();
     }
 
     @Override
     public void onDrawerItemSelected(View view, int position) {
 
+    }
+
+    private void populateListView() {
+        itens = new ArrayList<ListViewItem>();
+        ListViewItem item1 = new ListViewItem("Título do Arquivo", R.drawable.archive);
+        ListViewItem item2 = new ListViewItem("Título do Arquivo", R.drawable.archive);
+        ListViewItem item3 = new ListViewItem("Título do Arquivo", R.drawable.archive);
+        ListViewItem item4 = new ListViewItem("Título do Arquivo", R.drawable.archive);
+
+        itens.add(item1);
+        itens.add(item2);
+        itens.add(item3);
+        itens.add(item4);
+
+        listViewAdapter = new ListViewAdapter(this, itens);
+        listView.setAdapter(listViewAdapter);
     }
 }
---
+++ app/src/main/java/br/ufrn/imd/campusteca/adapter/ListViewAdapter.java
@@ -0,0 +1,50 @@
+package br.ufrn.imd.campusteca.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import br.ufrn.imd.campusteca.R;
+import br.ufrn.imd.campusteca.model.ListViewItem;
+
+/**
+ * Created by Pablo Gabriel on 07/10/2015.
+ */
+public class ListViewAdapter extends BaseAdapter {
+    private LayoutInflater mInflater;
+    private List<ListViewItem> itens;
+
+    public ListViewAdapter(Context context, List<ListViewItem> itens) {
+        this.mInflater = LayoutInflater.from(context);
+        this.itens = itens;
+    }
+
+    public int getCount() {
+        return itens.size();
+    }
+
+    public ListViewItem getItem(int position) {
+        return itens.get(position);
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        ListViewItem item = itens.get(position);
+
+        view = mInflater.inflate(R.layout.item_list_view, null);
+
+        ((TextView) view.findViewById(R.id.archiveTitleTextView)).setText(item.getTitle());
+        ((ImageView) view.findViewById(R.id.archiveImageView)).setImageResource(item.getImage());
+
+        return view;
+    }
+}
