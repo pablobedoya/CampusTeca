ff4c34bd9e34413340062e7ca91b85fd04c568cb
Pablo Gabriel <pablo.reds@hotmail.com>
2015-10-14 01:33:36 -0300

    creating create review activity #2

--- app/src/main/java/br/ufrn/imd/campusteca/ArchiveActivity.java
+++ app/src/main/java/br/ufrn/imd/campusteca/ArchiveActivity.java
@@ -1,5 +1,6 @@
 package br.ufrn.imd.campusteca;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,8 +21,8 @@ public class ArchiveActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(ArchiveActivity.this, CreateReviewActivity.class);
+                startActivity(intent);
             }
         });
     }
